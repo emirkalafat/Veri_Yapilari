1e4a8714a600b3c51734750524a01b8dcf1dcd5f
emirkalafat <emirklft@outlook.com>
2023-10-03 14:57:48 +0300

    person class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Person p1 = new Person("Hasan",30);
+        System.out.println("Hello World!");
+        p1.showOutput();
+    }
+}
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+class Person{
+    private final String name;
+    private final int age;
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    void showOutput() {
+        System.out.println(this.name);
+        System.out.println(this.age);
     }
 }
